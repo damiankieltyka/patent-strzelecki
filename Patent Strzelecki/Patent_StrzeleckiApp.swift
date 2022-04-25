ccf74b3b0a89126426f922e330cbf67076355fe2
Damian Kieltyka <damian.kieltyka@icloud.com>
2022-04-25 23:23:26 +0200

    Initial Commit

---
+++ Patent Strzelecki/Patent_StrzeleckiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Patent_StrzeleckiApp.swift
+//  Patent Strzelecki
+//
+//  Created by Damian Kiełtyka on 25/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Patent_StrzeleckiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
